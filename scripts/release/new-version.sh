bbf433f131e00478bc704c23eb0d26ad82b30467
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-08-11 17:41:29 +0400

    feat: Add svu tool; Update gorelease. golangci-lint, pkgsite tools (#91)
    * feat: Add svu tool
    
    * feat: Reactor release script to use svu and require release branch
    
    * feat: Bump github.com/goreleaser/goreleaser to v1.20.0
    
    * feat: Bump golang.org/x/pkgsite to v0.0.0-20230810193958-82f79ed16e88
    
    * feat: Bump github.com/golangci/golangci-lint to v1.54.1
    
    * docs: Update README

--- scripts/release/new-version.sh
+++ scripts/release/new-version.sh
@@ -2,9 +2,38 @@
 
 set -Eeuo pipefail
 
-SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_DIR="$(dirname "$0")"
+REPO_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
+SCRIPTS_DIR="${REPO_ROOT}/scripts"
+
+source "${SCRIPTS_DIR}/helpers-source.sh"
+
+APP=${APP_NAME}
+
+RELEASE_BRANCH=${RELEASE_BRANCH:-"release"}
+
+echo "${SCRIPT_NAME} is running fo ${APP}... "
+
+checkInstalled 'svu'
 
 echo "${SCRIPT_NAME} is running... "
+
+function requireReleaseBranch() {
+  err=0
+  branch=$(git branch --show-current)
+
+  echo "Current branch is: ${branch}"
+
+  if [[ ${branch} != "${RELEASE_BRANCH}" ]]; then
+    err=1
+  fi
+
+   if [[ ${err} == 1 ]]; then
+        echo >&2 "Please checkout to ${RELEASE_BRANCH} branch."
+        exit 1
+    fi
+}
 
 function require_clean_work_tree() {
   # Update the index
@@ -40,27 +69,22 @@ function menu() {
   printf "4 - Exit\n"
   read -r selection
 
-  SHORTCOMMIT="$(git rev-parse --short HEAD)"
-
-  PREV_VERSION="$(git tag | sort -V | tail -1)"
-  if [ -z "${PREV_VERSION}" ] || [ "${PREV_VERSION}" = "${SHORTCOMMIT}" ]; then
-    PREV_VERSION="v0.0.0"
-  fi
+  PREV_VERSION=$(svu current)
 
   echo "Current version: ${PREV_VERSION}"
 
   case "$selection" in
   1)
     printf "Major updates......\n"
-    NEW_VERSION=$(echo ${PREV_VERSION} | sed 's/\(.*v\)\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)/\2;\3;\4;\1/g' | sort -t';' -k 1,1n -k 2,2n -k 3,3n | tail -n 1 | awk -F';' '{printf "%s%d.%d.%d", $4, ($1+1),0,0 }')
+    NEW_VERSION=$(svu major)
     ;;
   2)
     printf "Run Minor update.........\n"
-    NEW_VERSION=$(echo ${PREV_VERSION} | sed 's/\(.*v\)\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)/\2;\3;\4;\1/g' | sort -t';' -k 1,1n -k 2,2n -k 3,3n | tail -n 1 | awk -F';' '{printf "%s%d.%d.%d", $4, $1,($2+1),0 }')
+    NEW_VERSION=$(svu minor)
     ;;
   3)
     printf "Patch update.........\n"
-    NEW_VERSION=$(echo ${PREV_VERSION} | sed 's/\(.*v\)\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)/\2;\3;\4;\1/g' | sort -t';' -k 1,1n -k 2,2n -k 3,3n | tail -n 1 | awk -F';' '{printf "%s%d.%d.%d", $4, $1,$2,($3 + 1) }')
+    NEW_VERSION=$(svu patch)
     ;;
   4)
     printf "Exit................................\n"
@@ -74,6 +98,9 @@ function menu() {
   esac
 
 }
+
+## Check if release branch
+requireReleaseBranch
 
 ## Check if git is clean
 require_clean_work_tree "create new version"
@@ -92,7 +119,7 @@ while true; do
   case $yn in
   [Yy]*)
 
-    git tag -a "${NEW_TAG}" -m "${NEW_TAG}" &&
+  git tag -a "${NEW_TAG}" -m "${NEW_TAG}" &&
       git push --tags
 
     break
