029d0c05b8db11ae68089c95d12faca13985b339
Oleg Balunenko <oleg.balunenko@gmail.com>
2024-12-20 21:04:17 +0400

    docs: Update readme

--- scripts/sync-vendor.sh
+++ scripts/sync-vendor.sh
@@ -13,49 +13,11 @@ echo "${SCRIPT_NAME} is running..."
 # Function to sync the vendor directory
 sync_vendor() {
   echo "Syncing vendor..."
-  rm -rf vendor
+  rm -rf ./vendor
   go mod tidy
   go mod vendor
 }
 
-# Function to check the status of the last command
-check_status() {
-  local err_msg="$1"
-
-  if [ $? -ne 0 ]; then
-    if [ -n "${err_msg}" ]; then
-      echo "${err_msg}"
-    fi
-    # Exit 255 to signal xargs to abort the process with code 1
-    exit 255
-  fi
-}
-
-# Function to sync a single dependency
-sync_dep() {
-  local dep="$1"
-
-  local tools_module
-  tools_module="$(grep '^module ' go.mod | awk '{print $2}')"
-
-  local raw_version
-  raw_version=$(go list -mod=readonly -m -f '{{if not .Indirect}}{{.Path}} {{.Version}}{{end}}' all | grep -v "^${tools_module}")
-
-  local version
-  version=$(echo "${raw_version}" | awk '{print $2}')
-
-  echo "[INFO]: Syncing ${dep}@${version}"
-
-  sync_vendor
-  check_status "[FAIL]: Failed to sync [${dep}@${version}]!"
-
-  echo "[SUCCESS]: Synced [${dep}@${version}] successfully."
-}
-
-# Export functions for use in subshells
-export -f sync_vendor
-export -f check_status
-export -f sync_dep
 
 # Function to sync dependencies
 sync_deps() {
@@ -64,8 +26,7 @@ sync_deps() {
 
   echo "[INFO]: Running install_deps for ${tools_module}"
 
-  go list -e -f '{{ join .Imports "\n" }}' -tags="tools" "${tools_module}" |
-    xargs -n 1 -P 0 -I {} bash -c 'sync_dep "$@"' _ {}
+  sync_vendor
 }
 
 # Function to sync all tools in the TOOLS_DIR
